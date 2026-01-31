5c64ddd15833b1161549f78468463253212a27c4
Susan Gasson <sgmaxx@gmail.com>
2026-01-30 20:50:50 -0500

    Client-side interactivity & dark theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // import Config file
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // configure site URL for RSS
 export default defineConfig({
-  site: "https://sgastro.netlify.app"
+  site: "https://sgastro.netlify.app",
+  integrations: [preact()]
 });
